051ab6717e589605f9cde7864a940db4ebfe7515
ELARBISABBAR <larbisbbar1234@gmail.com>
2025-05-27 21:36:43 +0100

    folder menu done with full html and css and full js

--- public/js/app.js
+++ public/js/app.js
@@ -7,3 +7,48 @@ btn.addEventListener("click", () => {
 loginBtn.addEventListener("click", () => {
   modal.style.display = "none"
 })
+
+let navLinks = document.querySelectorAll("nav-link")
+let tabPanes = document.querySelectorAll("tab-pane")
+navLinks.forEach((link, index) =>{
+    link.addEventListener("click", function (e) {
+        e.preventDefault()
+        
+        navLinks.forEach(nav => nav.classList.remove("active"))
+        tabPanes.forEach(pane => pane.classList.remove("show", "active", "fade"))
+
+        link.classList.add("active")
+
+        tabPanes[index].classList.add("show", "active", "fade")
+    })
+})
+
+// caroussel section menu
+
+let carousel = document.querySelector(".carousel3")
+let slides = document.querySelectorAll(".slide")
+
+let currentIndex2 = 0
+let total = slides.length
+let slideInterval = 3000
+
+function start(index){
+    if(index >= total) {
+        currentIndex2 = 0
+    } else if (index < 0) {
+        currentIndex2 = total - 1
+    }else {
+        currentIndex2 = index
+    }
+
+    carousel.style.transform = `translateX(-${100 * currentIndex2}%)`
+
+}
+
+function AutoSlide(){
+    setInterval(() => {
+      start(currentIndex2 + 1) 
+    }, slideInterval);
+}
+start(currentIndex2)
+AutoSlide()
